a5c54ee80dee7b45003ca48a028df907ef8a4638
drchibs <richarddennis789@gmail.com>
2021-09-05 08:57:01 +0100

    setup axios and tests folder

--- src/termii.js
+++ src/termii.js
@@ -1,7 +1,17 @@
+const axios = require('axios');
+/**
+ * Termii SDK for NodeJS
+ * @author Dennis R. Chibs
+ */
 export default class Termii {
 	constructor(sender_id, api_key) {
 		this.sender_id = sender_id;
 		this.api_key = api_key;
         this.base_url = 'https://termii.com/api/'
 	}
+
+    /**
+     * This API allows businesses send text messages to their customers across different messaging channels. 
+     * The API accepts JSON request payload and returns JSON encoded responses, and uses standard HTTP response codes.
+     */
 }
