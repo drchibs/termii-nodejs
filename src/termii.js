ddf522ae65fe2ee55b170fc5beb0f55ae48c6234
drchibs <richarddennis789@gmail.com>
2021-09-06 03:04:51 +0100

    fixing async await

--- src/termii.js
+++ src/termii.js
@@ -1,5 +1,5 @@
-const axios = require('axios').default;
-const status = require('./http_codes');
+const axios = require("axios").default;
+const status = require("./http_codes");
 /**
  * Termii SDK for NodeJS
  * @author Dennis R. Chibs
@@ -10,40 +10,37 @@ module.exports = class Termii {
 	constructor(sender_id, api_key) {
 		this.sender_id = sender_id;
 		this.api_key = api_key;
-        this.channel = "generic";
-        this.message_type  = "plain";
-        this.base_url = 'https://termii.com/api/'
+		this.channel = "generic";
+		this.message_type = "plain";
+		this.base_url = "https://termii.com/api/";
 	}
 
-    /**
-     * This API allows businesses send text messages to their customers across different messaging channels. 
-     * The API accepts JSON request payload and returns JSON encoded responses, and uses standard HTTP response codes.
-     * @param string recipient (Ex: 23490126727)
-     * @param string message 
-     * @return array
-     */
-    async sendMessage(recipient, message) {
-        try {
-            const request = await axios.post('sms/send', {
-                api_key: this.api_key,
-                to: recipient,
-                from: this.sender_id,
-                sms: message,
-                type: this.message_type,
-                channel: this.channel
-            });
-            //console.log(request);
-            let res = await JSON.stringify(request);
-            return res;
-
-        } catch (error) {
-            //console.error(error);
-            const res = {
-                status: status(error.status)
-            }
-            return JSON.stringify(res);
-        }
-    }
-    
-   
-}
+	/**
+	 * This API allows businesses send text messages to their customers across different messaging channels.
+	 * The API accepts JSON request payload and returns JSON encoded responses, and uses standard HTTP response codes.
+	 * @param string recipient (Ex: 23490126727)
+	 * @param string message
+	 * @return array
+	 */
+	async sendMessage(recipient, message) {
+		try {
+			const request = await axios.post("sms/send", {
+				api_key: this.api_key,
+				to: recipient,
+				from: this.sender_id,
+				sms: message,
+				type: this.message_type,
+				channel: this.channel,
+			});
+			//console.log(request);
+			if (request.status === 200) {
+				let res = JSON.stringify(request);
+				return res;
+			}
+			return status(request.status);
+		} catch (error) {
+			//console.error(error);
+			return JSON.stringify(status(error.status));
+		}
+	}
+};
