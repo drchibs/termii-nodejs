ef0dc75b6ac7a354b052192c007a09eddb14e39a
drchibs <richarddennis789@gmail.com>
2021-09-06 06:01:25 +0100

    sendToken written, without the test though

--- src/termii.js
+++ src/termii.js
@@ -77,4 +77,45 @@ module.exports = class Termii {
 			return err.message;
 		}
 	}
+
+
+    /**
+     * The send token API allows businesses trigger one-time-passwords (OTP) across any available messaging channel on Termii. One-time-passwords created are generated randomly and there's an option to set an expiry time.
+     * @param {string} phone_number 
+     * @param {enum} message_type ALPHANUMERIC or NUMERIC
+     * @param {string} channel either dnd, WhatsApp, or generic (we chose generic automatically for you)
+     * @param {number} pin_attempts 
+     * @param {number} pin_time (in minutes between 1 - 60)
+     * @param {number} pin_length 
+     * @param {string} pin_placeholder Ex: "< 1234 >" (Refer to docs)
+     * @param {string} message_text Ex: "Your pin is < 1234 >"
+     * @returns 
+     */
+    async sendToken(number, message_type="ALPHANUMERIC", channel=null, attempts=3, pin_time=1, pin_length=4, pin_placeholder, message_text) {
+		let url = this.base_url + "sms/otp/generate";
+		try {
+			const request = await fetch(url, {
+				method: "POST",
+				body: JSON.stringify({
+					api_key: this.api_key,
+					message_type: message_type,
+					to: number,
+                    from: this.sender_id,
+                    channel: channel || this.channel,
+					pin_attempts: attempts,
+					pin_time_to_live: pin_time,
+					pin_length: pin_length,
+                    pin_placeholder: pin_placeholder,
+                    message_text: message_text
+				}),
+                headers: { 'Content-Type': 'application/json' }
+			});
+			const response = await request.json();
+			return response;
+		} catch (err) {
+			//console.log(err);
+			return err.message;
+		}
+	}
+    
 };
