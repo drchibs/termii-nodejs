8c846e821ac2a54820c8caef6e5a52df83c26f40
drchibs <richarddennis789@gmail.com>
2021-09-06 06:35:09 +0100

    refactor base class to use optional construcor parameters

--- src/termii.js
+++ src/termii.js
@@ -7,11 +7,15 @@ const status = require("./http_codes");
  * @param string api_key
  */
 module.exports = class Termii {
-	constructor(sender_id, api_key) {
-		this.sender_id = sender_id;
-		this.api_key = api_key;
-		this.channel = "generic";
-		this.message_type = "plain";
+	constructor(options = {}) {
+		this.sender_id = options.sender_id;
+		this.api_key = options.api_key;
+		this.channel = (options && options.channel) || "generic";
+		this.message_type = (options && options.message_type) || "plain";
+		this.pin_attempts = (options && options.pin_attempts) || 3;
+		this.pin_time = (options && options.pin_time) || 1;
+		this.pin_length = (options && options.pin_length) || 4;
+		this.pin_type = (options && options.pin_type) || "NUMERIC";
 		this.base_url = "https://termii.com/api/";
 	}
 
@@ -35,7 +39,7 @@ module.exports = class Termii {
 					type: this.message_type,
 					channel: this.channel,
 				}),
-                headers: { 'Content-Type': 'application/json' }
+				headers: { "Content-Type": "application/json" },
 			});
 			const response = await request.json();
 			return response;
@@ -45,30 +49,30 @@ module.exports = class Termii {
 		}
 	}
 
-    /**
-     * This API returns OTP codes in JSON format which can be used within any web or mobile app. Tokens are numeric or alpha-numeric codes generated to authenticate login requests and verify customer transactions.
-     * @param {string} phone_number 
-     * @param {enum} pin_type  ALPHANUMERIC or NUMERIC
-     * @param {number} pin_attempts 
-     * @param {number} pin_time  (in minutes 1-60)
-     * @param {number} pin_length 
-     * @returns object 
-     */
+	/**
+	 * This API returns OTP codes in JSON format which can be used within any web or mobile app. Tokens are numeric or alpha-numeric codes generated to authenticate login requests and verify customer transactions.
+	 * @param {string} phone_number
+	 * @param {enum} pin_type  ALPHANUMERIC or NUMERIC
+	 * @param {number} pin_attempts
+	 * @param {number} pin_time  (in minutes 1-60)
+	 * @param {number} pin_length
+	 * @returns object
+	 */
 
-    async inAppToken(number, pin_type="ALPHANUMERIC",  attempts=3, pin_time=1, pin_length=4) {
+	async inAppToken(number) {
 		let url = this.base_url + "sms/otp/generate";
 		try {
 			const request = await fetch(url, {
 				method: "POST",
 				body: JSON.stringify({
 					api_key: this.api_key,
-					pin_type: pin_type,
+					pin_type: this.pin_type,
 					phone_number: number,
-					pin_attempts: attempts,
-					pin_time_to_live: pin_time,
-					pin_length: pin_length
+					pin_attempts: this.pin_attempts,
+					pin_time_to_live: this.pin_time,
+					pin_length: this.pin_length,
 				}),
-                headers: { 'Content-Type': 'application/json' }
+				headers: { "Content-Type": "application/json" },
 			});
 			const response = await request.json();
 			return response;
@@ -78,37 +82,36 @@ module.exports = class Termii {
 		}
 	}
 
-
-    /**
-     * The send token API allows businesses trigger one-time-passwords (OTP) across any available messaging channel on Termii. One-time-passwords created are generated randomly and there's an option to set an expiry time.
-     * @param {string} phone_number 
-     * @param {enum} message_type ALPHANUMERIC or NUMERIC
-     * @param {string} channel either dnd, WhatsApp, or generic (we chose generic automatically for you)
-     * @param {number} pin_attempts 
-     * @param {number} pin_time (in minutes between 1 - 60)
-     * @param {number} pin_length 
-     * @param {string} pin_placeholder Ex: "< 1234 >" (Refer to docs)
-     * @param {string} message_text Ex: "Your pin is < 1234 >"
-     * @returns 
-     */
-    async sendToken(number, message_type="ALPHANUMERIC", channel=null, attempts=3, pin_time=1, pin_length=4, pin_placeholder, message_text) {
-		let url = this.base_url + "sms/otp/generate";
+	/**
+	 * The send token API allows businesses trigger one-time-passwords (OTP) across any available messaging channel on Termii. One-time-passwords created are generated randomly and there's an option to set an expiry time.
+	 * @param {string} phone_number
+	 * @param {enum} message_type ALPHANUMERIC or NUMERIC
+	 * @param {string} channel either dnd, WhatsApp, or generic (we chose generic automatically for you)
+	 * @param {number} pin_attempts
+	 * @param {number} pin_time (in minutes between 1 - 60)
+	 * @param {number} pin_length
+	 * @param {string} pin_placeholder Ex: "< 1234 >" (Refer to docs)
+	 * @param {string} message_text Ex: "Your pin is < 1234 >"
+	 * @returns
+	 */
+	async sendToken(number, pin_placeholder, message_text) {
+		let url = this.base_url + "sms/otp/send";
 		try {
 			const request = await fetch(url, {
 				method: "POST",
 				body: JSON.stringify({
 					api_key: this.api_key,
-					message_type: message_type,
+					message_type: this.pin_type,
 					to: number,
-                    from: this.sender_id,
-                    channel: channel || this.channel,
-					pin_attempts: attempts,
-					pin_time_to_live: pin_time,
-					pin_length: pin_length,
-                    pin_placeholder: pin_placeholder,
-                    message_text: message_text
+					from: this.sender_id,
+					channel: this.channel,
+					pin_attempts: this.pin_attempts,
+					pin_time_to_live: this.pin_time,
+					pin_length: this.pin_length,
+					pin_placeholder: pin_placeholder,
+					message_text: message_text,
 				}),
-                headers: { 'Content-Type': 'application/json' }
+				headers: { "Content-Type": "application/json" },
 			});
 			const response = await request.json();
 			return response;
@@ -117,5 +120,26 @@ module.exports = class Termii {
 			return err.message;
 		}
 	}
-    
+
+	async sendVoiceToken(number) {
+		let url = this.base_url + "sms/otp/send/voice";
+		try {
+			const request = await fetch(url, {
+				method: "POST",
+				body: JSON.stringify({
+					api_key: this.api_key,
+					phone_number: number,
+					pin_attempts: this.pin_attempts,
+					pin_time_to_live: this.pin_time,
+					pin_length: this.pin_length,
+				}),
+				headers: { "Content-Type": "application/json" },
+			});
+			const response = await request.json();
+			return response;
+		} catch (err) {
+			//console.log(err);
+			return err.message;
+		}
+	}
 };
--- tests/inapptoken.js
+++ tests/inapptoken.js
@@ -5,10 +5,14 @@ const sender_id = "octopii";
 const api_key =
 	"TLPMOToGfquQFrPyIddzOS8p5eIWdIK9w8B98ru7yAW3pHoOY80cdWUIG7NpJG";
 
-const tel = "2348090897047";
+const tel = "2348090897046";
 const sms = "Hello, World!";
 
-const termii = new Termii(sender_id, api_key);
-const task = termii.inAppToken(tel).then(res => console.log(res));
+const termii = new Termii({
+	api_key: api_key,
+	sender_id: sender_id,
+	pin_attempts: 4,
+});
+const task = termii.inAppToken(tel).then((res) => console.log(res));
 // const res = termii.inAppToken(tel);
 // console.log(res)
